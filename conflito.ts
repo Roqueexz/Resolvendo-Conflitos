21865a6f10de7555b2830c3f6b30ba61c731d8a1
PABLO ISAAC SPONCHIADO SILVA <pablo.sponchiado@portalsesisp.org.br>
2026-03-06 10:57:25 -0300

    alunoB inserindo linha6 vazia e linha7 com código

--- conflito.ts
+++ conflito.ts
@@ -1,7 +1,7 @@
 console.log(`alunoA alterou a linha 1 do arquivo conflito.ts`);
 console.log(`alunoB inserindo a linha 2 `);
-
 let calculo: number = (75 + 75) / 3; // calculo qualquer
-
 let concatenar: string = `Git` + `Hub`;
 console.log(`Concatenado ${concatenar}`);
+
+console.log(`Inserindo linha 7 pelo alunoB`);
